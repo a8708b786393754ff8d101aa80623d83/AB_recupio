45b799c81c9e40e5e1734f3724503886f33e5753
a6888b <ayoubbrahim68@gmail.com>
2022-07-04 21:52:24 +0200

    formatage

--- script.py
+++ script.py
@@ -1,22 +1,25 @@
 #! /usr/local/bin/python3.10
-from pathlib import Path 
+from pathlib import Path
 import projet_fini.AB_recupio.arguments as arguments
 
-def computer_list(): 
+
+def computer_list():
     """Liste tout les fichier est dossier a partir de la racine
 
     Yields:
         Path: fichier ou dossier 
     """
-    
-    for element in Path('/').rglob("**"): 
+
+    for element in Path('/').rglob("**"):
         yield element
-        
+
 
 args = arguments.args()
 
-for element in computer_list(): 
-    if Path(element).is_file() and args.: 
-        pass 
-    elif Path(element).is_dir(): 
-        pass 
+for element in computer_list():
+    if not args.folder and not args.file:
+        pass
+    elif Path(element).is_file() and args.file:
+        pass
+    elif Path(element).is_dir() and args.folder:
+        pass
