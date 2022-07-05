c462bbbeac1d408357972a0a43fcbfcfe4922cb4
a6888b <ayoubbrahim68@gmail.com>
2022-07-05 22:12:23 +0200

    reformatage

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import arguments as arguments
 
 
-def computer_list(file: bool=False, folder: bool=False):
+def computer_list(file: bool = False, folder: bool = False):
     """Liste tout les fichier est dossier a partir de la racine
 
     Yields:
@@ -11,32 +11,32 @@ def computer_list(file: bool=False, folder: bool=False):
     """
 
     for element in Path('/').rglob("**"):
-        if file and element.is_file(): 
+        if file and element.is_file():
             yield element
-        
-        elif folder and element.is_dir(): 
-            yield element
-            
-        elif not folder and not file: 
-            yield element
-            
 
-def search(file: str|Path, research: str) -> bool: 
-    if isinstance(file, Path): 
+        elif folder and element.is_dir():
+            yield element
+
+        elif not folder and not file:
+            yield element
+
+
+def search(file: str | Path, research: str) -> bool:
+    if isinstance(file, Path):
         file = str(file)
-        
-    return file.startswith(research) or file.endswith(research) or file == research or file in research
+
+    return (file.startswith(research) or file.endswith(research)) or (file == research or file in research)
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     args = arguments.args()
 
-    if args.folder: 
-        for folder in computer_list(folder=True): 
-            if search(folder, args.folder): 
+    if args.folder:
+        for folder in computer_list(folder=True):
+            if search(folder, args.folder):
                 print(folder)
-            
-    elif args.file: 
-        for file in computer_list(file=True): 
-            if search(file, args.file): 
+
+    elif args.file:
+        for file in computer_list(file=True):
+            if search(file, args.file):
                 print(file)
